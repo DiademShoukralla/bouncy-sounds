0fcce42110691dea0668e5d30f33be688a889778
Diadem Shoukralla <diadem.shoukralla@mail.utoronto.ca>
2022-03-12 01:24:52 -0500

    converted x,y to vectors

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 let debugText = document.getElementById("debug");
 
+var hit = false;
+
+let lineCenterPos = {x: 250, y: 400}
+let linePos = [
+    {x: 50, y:150},
+    {x: 450, y: 150}
+]
+let lines, lineCenter
+
+let ballRadius = 15;
+let ballPos = [
+    {x: 100, y: 50}
+]
+let balls
+
 function setup() {
     collideDebug(true);
     createCanvas(500, 500);
     background(255);
+
+    lineCenter = createVector(lineCenterPos.x, lineCenterPos.y)
+    lines = linePos.map(({x, y}) => createVector(x, y));
+    balls = ballPos.map(({x,y}) => ({pos: createVector(x, y), dir: 180}))
 }
 
-var hit = false;
-
-let lineCenter = {x: 250, y: 400}
-let lines = [
-    {x: 50, y: 150},
-    {x: 450, y: 150}
-]
-
-let ballRadius = 15;
-let balls = [
-    {x: 100, y: 50, dir: 180}
-]
-
-// Return True iff point at (testX,testY) is colliding with either line
-function didHit(testX, testY){
-    return lines
-        .some(({x,y}) => collidePointLine(testX, testY, x, y, lineCenter.x, lineCenter.y));
+/**
+ * If a collision occurs with inVec, return the line vector in which it occured with
+ *  */
+function didHit(inVec){
+    for(let i=0; i<lines.length; i++){
+        if(collidePointLineVector(inVec, lines[i], lineCenter)) return lines[i];
+    }
+    return null
 }
 
 function draw() {
@@ -31,18 +41,21 @@ function draw() {
 
     // Draw lines
     stroke(color('black'));
-    lines.forEach(({x, y}) => {
-        line(x, y, lineCenter.x, lineCenter.y);
+    lines.forEach(lineVec => {
+        line(lineVec.x, lineVec.y, lineCenter.x, lineCenter.y);
     })
 
     // Draw balls
-    balls.forEach(({x,y}) => {
-        circle(x, y, ballRadius)
+    balls.forEach(({pos}) => {
+        circle(pos.x, pos.y, ballRadius)
         // Test draw the next one
     })
 
     // Calculate hit
-    hit = didHit(mouseX, mouseY);
+    hit = didHit(createVector(mouseX, mouseY));
     // update based on hit
-    if(hit) debugText.textContent = "Hit!"
+    if(hit) {
+        debugText.textContent = "Hit!"
+        
+    }
 }
